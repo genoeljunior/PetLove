2032855c2baca2dadfd5dbe9861f43316354fb1e
Rayzev <rayldoazevedo@gmail.com>
2021-11-10 22:56:50 -0300

    alt login

--- frontPetLove-master/components/Login/index.js
+++ frontPetLove-master/components/Login/index.js
@@ -7,25 +7,24 @@ const Login = () =>(
     <Container  className="imglog">
     <Row className="linhalog">
       <Col className="tdlog">
-        <Image src="/log1.jpg" width={570} height={778} />
+        <Image src="/log1.jpg" width={370} height={458} />
         </Col>
         <Col >
             <h1>login</h1>
             <form>
             <div className="mb-3">
                 <label for="exampleInputEmail1" class="form-label-sm">Nome</label>
-                <input type="Nome" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="logNome"/>
-                <div id="logNome" class ="form-text">Digite seu nome.</div>
+                <input type="Nome" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="logNome" placeholder="Digite sem nome"/>
             </div>
 
             <div className="mb-3">
-                <label for="exampleInputPassword1" class="form-label-sm">Password</label>
+                <label for="exampleInputPassword1" class="form-label-sm">Senha</label>
                 <input type="password" class="form-control form-control-sm" id="exampleInputPassword1"/>
             </div>
 
             <div className="mb-3 form-check">
                 <input type="checkbox"  id="exampleCheck1"  class="form-check-input" id="exampleCheck1"/>
-                <label for="exampleCheck1" class ="form-check-label">Check me out</label>
+                <label for="exampleCheck1" class ="form-check-label">Mantenha conectado</label>
             </div>
             <div className="d-grid gap-2">
                 <button type="submit" class="btn btn-primary btvejamais lg">Entrar</button>
